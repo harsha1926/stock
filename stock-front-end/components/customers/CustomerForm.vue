95cf9b06221cdc682f501908aaf39f25ae97a658
Sriharsha Samana <sriharsha.samana@gmail.com>
2019-09-27 22:01:02 -0400

    Implemented create form using props and emit

---
+++ stock-front-end/components/customers/CustomerForm.vue
@@ -0,0 +1,137 @@
+<template>
+  <div>
+    <v-dialog :value="dialog" persistent max-width="600px">
+      <v-form v-model="valid" ref="form">
+        <v-card>
+          <v-card-title>
+            <span class="headline">{{$t('app.actions.add_new_customer')}}</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container grid-list-md>
+              <v-layout wrap>
+                <v-flex xs12 sm6 md6>
+                  <v-text-field
+                    :label="$t('app.actions.name')"
+                    v-model="name"
+                    :rules="[rules.required, rules.minNameLength, rules.maxNameLength]"
+                    counter="50"
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm6 md6>
+                  <v-text-field :label="$t('app.actions.reference')" v-model="reference"></v-text-field>
+                </v-flex>
+                <v-flex xs12>
+                  <v-text-field
+                    :label="$t('app.actions.address_1')"
+                    v-model="address1"
+                    :rules="[rules.required, rules.maxAddressLength]"
+                    counter="50"
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs12>
+                  <v-text-field :label="$t('app.actions.address_2')" v-model="address2"></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm6 md6>
+                  <v-autocomplete
+                    :label="$t('app.actions.country')"
+                    v-model="country"
+                    :rules="[rules.required]"
+                    :items="['Canada']"
+                  ></v-autocomplete>
+                </v-flex>
+                <v-flex xs12 sm6 md6>
+                  <v-autocomplete
+                    :label="$t('app.actions.state')"
+                    v-model="state"
+                    :rules="[rules.required]"
+                    :items="['Quebec']"
+                  ></v-autocomplete>
+                </v-flex>
+                <v-spacer></v-spacer>
+                <v-flex xs12 sm6 md6>
+                  <v-autocomplete
+                    :label="$t('app.actions.city')"
+                    v-model="city"
+                    :rules="[rules.required]"
+                    :items="['Longueuil']"
+                  ></v-autocomplete>
+                </v-flex>
+                <v-flex xs12 sm6 md6>
+                  <v-autocomplete
+                    :label="$t('app.actions.postal_code')"
+                    v-model="postalCode"
+                    :rules="[rules.required]"
+                    :items="['J4L4A8']"
+                  ></v-autocomplete>
+                </v-flex>
+                <v-flex xs12 sm6 md6>
+                  <v-text-field
+                    :label="$t('app.actions.email')"
+                    v-model="email"
+                    :rules="[rules.emailRule]"
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm6 md6>
+                  <v-text-field
+                    :label=" $t('app.actions.phone')"
+                    v-model="phone"
+                    mask="phone"
+                    :rules="[rules.required]"
+                  ></v-text-field>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary darken-1" flat @click="dialogClosed()">Close</v-btn>
+            <v-btn color="primary darken-1" flat :disabled="!valid" @click="addNewCustomer()">Save</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-form>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    dialog: Boolean
+  },
+  data() {
+    return {
+      valid: false,
+      name: null,
+      reference: null,
+      address1: null,
+      address2: null,
+      country: null,
+      state: null,
+      city: null,
+      postalCode: null,
+      email: null,
+      phone: null,
+
+      rules: {
+        required: v => !!v || this.$t('app.actions.validations.required'),
+        minNameLength: v =>
+          (v && v.length > 2) ||
+          this.$t('app.actions.validations.min_name_length'),
+        maxNameLength: v =>
+          (v && v.length <= 50) ||
+          this.$t('app.actions.validations.max_name_length'),
+        maxAddressLength: v =>
+          (v && v.length <= 50) ||
+          this.$t('app.actions.validations.max_address_length'),
+        emailRule: v =>
+          /.+@.+/.test(v) || this.$t('app.actions.validations.email')
+      }
+    }
+  },
+  methods: {
+    dialogClosed: function() {
+      this.$emit('dialog-closed')
+    }
+  }
+}
+</script>
--- stock-front-end/components/customers/Customers.vue
+++ stock-front-end/components/customers/Customers.vue
@@ -1,75 +1,89 @@
 <template>
-  <v-container fluid>
+  <v-container fluid ma-0 pa-0>
     <v-snackbar bottom color="primary" v-model="snackbar">{{ snackbarMessage }}</v-snackbar>
-    <v-layout>
-      <v-flex xs12 sm12 md6 offset-md3 la4 offset-la4 xl4 offset-xl4>
-        <v-card>
-          <v-layout>
-            <v-layout>
-              <v-card-title class="title">{{ $t('app.actions.customers') }}</v-card-title>
-            </v-layout>
-          </v-layout>
-          <v-card-text>
-            <v-layout justify-center>
-              <span v-if="loading">
-                <v-progress-circular color="secondary" indeterminate></v-progress-circular>
-                <span>Please wait..</span>
-              </span>
-              <v-list three-line>
-                <v-list-tile v-for="customer in customers" :key="customer.id">
-                  <v-list-tile-content>
-                    <v-list-tile-title>
-                      {{ customer.name }}
-                      <span
-                        class="caption font-weight-thin"
-                      >{{ customer.reference }}</span>
-                    </v-list-tile-title>
-                    <v-list-tile-sub-title>{{ customer.address }}</v-list-tile-sub-title>
-                  </v-list-tile-content>
+    <v-flex xs12 sm12 md6 offset-md3 la4 offset-la4 xl4 offset-xl4>
+      <v-card>
+        <v-layout pt-3>
+          <customer-form :dialog="showCreateDialog" @dialog-closed="showCreateDialog=false" />
+          <v-flex hidden-xs-only>
+            <v-btn fab color="primary" icon small @click="showCreateDialog=true">
+              <v-icon>add</v-icon>
+            </v-btn>
+          </v-flex>
+          <v-flex hidden-sm-and-up>
+            <v-btn fab color="primary" bottom right fixed icon small @click="showCreateDialog=true">
+              <v-icon>add</v-icon>
+            </v-btn>
+          </v-flex>
 
-                  <v-list-tile-avatar>
-                    <v-icon color="primary">phone</v-icon>
-                  </v-list-tile-avatar>
-                  <v-list-tile-avatar>
-                    <v-menu bottom left>
-                      <template v-slot:activator="{ on }">
-                        <v-icon color="primary" v-on="on">more_vert</v-icon>
-                      </template>
-                      <v-list>
-                        <v-list-tile>
-                          <v-list-tile-avatar>
-                            <v-icon color="primary">email</v-icon>
-                          </v-list-tile-avatar>
-                        </v-list-tile>
-                        <v-list-tile>
-                          <v-list-tile-avatar>
-                            <v-icon color="primary">edit</v-icon>
-                          </v-list-tile-avatar>
-                        </v-list-tile>
-                        <v-list-tile>
-                          <v-list-tile-avatar>
-                            <v-icon
-                              color="primary"
-                              @click="deleteSelectedCustomer(customer)"
-                              v-on="on"
-                            >delete</v-icon>
-                          </v-list-tile-avatar>
-                        </v-list-tile>
-                      </v-list>
-                    </v-menu>
-                  </v-list-tile-avatar>
-                </v-list-tile>
-              </v-list>
-            </v-layout>
-          </v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout>
+          <v-layout>
+            <v-card-title class="title">{{ $t('app.actions.customers') }}</v-card-title>
+          </v-layout>
+        </v-layout>
+        <v-card-text>
+          <v-layout justify-center>
+            <span v-if="loading">
+              <v-progress-circular color="secondary" indeterminate></v-progress-circular>
+              <span>Please wait..</span>
+            </span>
+            <v-list three-line>
+              <v-list-tile v-for="customer in customers" :key="customer.id">
+                <v-list-tile-content>
+                  <v-list-tile-title>
+                    {{ customer.name }}
+                    <span
+                      class="caption font-weight-thin"
+                    >{{ customer.reference }}</span>
+                  </v-list-tile-title>
+                  <v-list-tile-sub-title>{{ customer.address }}</v-list-tile-sub-title>
+                </v-list-tile-content>
+
+                <v-list-tile-avatar>
+                  <v-icon color="primary">phone</v-icon>
+                </v-list-tile-avatar>
+                <v-list-tile-avatar>
+                  <v-menu bottom left>
+                    <template v-slot:activator="{ on }">
+                      <v-icon color="primary" v-on="on">more_vert</v-icon>
+                    </template>
+                    <v-list>
+                      <v-list-tile>
+                        <v-list-tile-avatar>
+                          <v-icon color="primary">email</v-icon>
+                        </v-list-tile-avatar>
+                      </v-list-tile>
+                      <v-list-tile>
+                        <v-list-tile-avatar>
+                          <v-icon color="primary">edit</v-icon>
+                        </v-list-tile-avatar>
+                      </v-list-tile>
+                      <v-list-tile>
+                        <v-list-tile-avatar>
+                          <v-icon
+                            color="primary"
+                            @click="deleteSelectedCustomer(customer)"
+                            v-on="on"
+                          >delete</v-icon>
+                        </v-list-tile-avatar>
+                      </v-list-tile>
+                    </v-list>
+                  </v-menu>
+                </v-list-tile-avatar>
+              </v-list-tile>
+            </v-list>
+          </v-layout>
+        </v-card-text>
+      </v-card>
+    </v-flex>
   </v-container>
 </template>
 <script>
 import { getCustomers, postCustomer, deleteCustomer } from '~/api/customers'
+import CustomerForm from './CustomerForm'
 export default {
+  components: {
+    CustomerForm
+  },
   data() {
     return {
       customers: [],
@@ -89,6 +103,7 @@ export default {
       snackbar: false,
       on: false,
       snackbarMessage: null,
+      showCreateDialog: false,
       rules: {
         required: v => !!v || this.$t('app.actions.validations.required'),
         minNameLength: v =>
