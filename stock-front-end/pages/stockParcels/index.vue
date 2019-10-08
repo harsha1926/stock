7f6b8996f63eeee533b78b57ea200bce819406c9
Neelima Nandam <neelimanandam@gmail.com>
2019-10-08 12:12:33 -0400

    added new folder for stockParcels in pages

---
+++ stock-front-end/pages/stockParcels/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-content>
+    <stock-parcels />
+  </v-content>
+</template>
+
+<script>
+import StockParcels from '~/components/stockParcels/StockParcels'
+export default {
+  components: {
+    StockParcels
+  },
+  layout: 'default'
+}
+</script>
